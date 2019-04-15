0bc17ee1be61a3c9e92dbc8ca4ea896260b3b862
omajzoub <omajzoub2013@gmail.com>
2019-04-15 17:07:10 -0500

    Finished

---
+++ assignment1.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+
+    console.log('Assignment 1');
+    var c = 'my variable';
+    console.log(c);
+    //console.log(document);
+    console.log('Id');
+    //this works
+
+    var mydiv = $("#Id");
+    console.log(mydiv);
+    mydiv.setAttribute('style', 'color: red');
+
+
+    console.log($assignment1.css("color"));
+
+    console.log(div);
+
+    // $( document ).ready(function()){
+                       // console.log('Hello');
+
+                        // });
+    });
